7985273978b5415ed4b0efdac93814498bb21044
naimsheikh8020 <naimsheikh8020@gmail.com>
2026-07-27 04:41:13 +0600

    implement ProjectCard component and integrate it into the Community page

--- Frontend/src/components/ProjectCard.tsx
+++ Frontend/src/components/ProjectCard.tsx
@@ -1,7 +1,34 @@
+import { useState, type Dispatch } from "react"
+import type { Project } from "../types"
+import type React from "react"
+import { useNavigate } from "react-router-dom"
 
-const ProjectCard = () => {
+const ProjectCard = ({gen, setGeneration, forCommunity = false}: {gen: Project, setGeneration: Dispatch<React.SetStateAction<Project[]>>, forCommunity?:boolean}) => {
+  const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false)
   return (
-    <div>ProjectCard</div>
+    <div key={gen.id} className="mb-4 break-inside-avoid">
+      <div className="bg-white/5 border border-white/10 rounded-xl overflow-hidden hover:border-white/20 transition group">
+      {/* preview */}
+      <div className={`${gen?.aspectRatio === '9:16' ? 'aspect-9/16': 'aspect-video'} relative overflow-hidden`}>
+        {
+          gen.generatedImage && (
+            <img src={gen.generatedImage} alt={gen.productName} className={`absolute inset-0 w-full h-full object-cover transition duration-500 ${gen.generatedVideo ? 'group-hover:opacity-0' : 'group-hover:scale-105'}`}/>
+          )
+        }
+        {
+          gen.generatedVideo && (
+            <video src={gen.generatedVideo} muted loop playsInline className="absolute inset-0 w-full h-full object-cover opacity-0 group-hover:opacity-100 transition duration-500"
+            onMouseEnter={(e)=>e.currentTarget.play()}
+            onMouseLeave={(e)=> e.currentTarget.pause()}/>
+          )
+        }
+      </div>
+
+      {/* Details */}
+      <div></div>
+      </div>
+    </div>
   )
 }
 
--- Frontend/src/pages/Community.tsx
+++ Frontend/src/pages/Community.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import type { Project } from "../types"
 import { dummyGenerations } from "../assets/assets"
 import { Loader2Icon } from "lucide-react"
+import ProjectCard from "../components/ProjectCard"
 
 const Community = () => {
   const [projects, setProject] = useState<Project[]>([])
@@ -32,7 +33,7 @@ const Community = () => {
         {/* Project List */}
         <div className="columns-1 sm:columns-2 lg:columns-3 gap-4">
           {projects.map((project) => {
-            return <div key={project.id}>{project.id}</div>;
+            return <ProjectCard key={project.id} gen={project} setGeneration={setProject} forCommunity={true}/>
           })}
         </div>
       </div>
