a44b4739bdc67c1eb6c09b70987592b5f44269f1
naimsheikh8020 <naimsheikh8020@gmail.com>
2026-07-27 00:37:20 +0600

    edit UploadZone component to display file preview and clear button

--- Frontend/src/components/UploadZone.tsx
+++ Frontend/src/components/UploadZone.tsx
@@ -1,3 +1,4 @@
+import { XIcon } from "lucide-react"
 import type { UploadZoneProps } from "../types"
 
 const UploadZone = ({ label, file, onClear, onChange }: UploadZoneProps) => {
@@ -5,7 +6,17 @@ const UploadZone = ({ label, file, onClear, onChange }: UploadZoneProps) => {
     <div className="relative group">
       <div className={`relative h-64 rounded-2xl border-2 border-dashed transition-all duration-300 flex flex-col items-center justify-center bg-white/2 p-6 ${file ? "border-violet-600/50 bg-violet-500/5" : "border-white/10 hover:border-violet-500/30 hover:bg-white/5"}`}>
         {
-          file ? (<></>): (<></>)
+          file ? (<>
+          <img src={URL.createObjectURL(file)} alt="Preview" className="absolute inset-0 w-full h-full object-cover rounded-xl opacity-60"/>
+          <div className="absolute inset-0 flex  items-center justify-center opacity-0 group-hover:opacity-100 transtiion-opacity bg-black/40 rounded-xl backdrop-blur-sm">
+            <button type="button" onClick={onClear} className="p-2 rounded-full bg-white/10 hover:bg-red-500/20 text-white hover:text-red-4000 transition-colors">
+              <XIcon className="w-6 h-6" />
+             </button>
+          </div>
+          <div className="absolute bottom-4 left-4 right-4 bg-black/50 backdrop-blur-md p-3 rounded-lg border border-white/10">
+            <p className="text-white text-sm truncate">{file?.name}</p>
+          </div>
+          </>): (<></>)
         }
       </div>
     </div>
