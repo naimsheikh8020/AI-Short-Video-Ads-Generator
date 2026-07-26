1b6c0268d5630b2a92e947ba6eb35412159ba457
naimsheikh8020 <naimsheikh8020@gmail.com>
2026-07-27 02:25:42 +0600

    add the type of project

--- Frontend/src/pages/Community.tsx
+++ Frontend/src/pages/Community.tsx
@@ -1,3 +1,4 @@
+
 const Community = () => {
   return (
     <div>Community</div>
--- Frontend/src/types/index.ts
+++ Frontend/src/types/index.ts
@@ -1,8 +1,34 @@
-import type React from 'react';
+import type React from "react";
 
 export interface UploadZoneProps {
-    label: string;
-    file: File | null;
-    onClear: () => void;
-    onChange: (E: React.ChangeEvent<HTMLInputElement>) => void;
+  label: string;
+  file: File | null;
+  onClear: () => void;
+  onChange: (E: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export interface User {
+  id?: string;
+  name?: string;
+  email?: string;
+}
+
+export interface Project {
+  id: string;
+  name?: string;
+  userId?: string;
+  user?: User;
+  productName: string;
+  productDescription?: string;
+  userPrompt?: string;
+  aspectRatio: string;
+  targetLength: string;
+  generatedImage?: string;
+  generatedVideo?: string;
+  isGenerating: boolean;
+  isPublished: boolean;
+  error?: string;
+  createdAt: Date | string;
+  updateAt?: Date | string;
+  uploadImages: string[];
 }
