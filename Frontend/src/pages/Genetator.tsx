fb00847862b4da905fa091b95c70e032983ce698
naimsheikh8020 <naimsheikh8020@gmail.com>
2026-07-27 01:42:49 +0600

    fix the nameing of Generator and add the input field and Aspect Ratio select

--- Frontend/src/pages/Genetator.tsx
+++ Frontend/src/pages/Genetator.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react"
 import Title from "../components/Title"
 import UploadZone from "../components/UploadZone"
+import { RectangleHorizontal, RectangleVertical } from "lucide-react"
 
-const Genetator = () => {
+const Generator = () => {
   const [name, setName] = useState('')
   const [productName, setProductName] = useState('')
   const [productDescription, setProductDescription] = useState('')
@@ -16,8 +17,8 @@ const Genetator = () => {
     e: React.ChangeEvent<HTMLInputElement>,
     type: 'product' | 'model'
   ) => {
-    if(e.target.files && e.target.files[0]) {
-      if(type === 'product') {
+    if (e.target.files && e.target.files[0]) {
+      if (type === 'product') {
         setProductImage(e.target.files[0])
       } else {
         setModelImage(e.target.files[0])
@@ -25,26 +26,49 @@ const Genetator = () => {
     }
   }
 
-  const handelGenerate = async (e: React.FormEvent<HTMLFormElement>) => {
-     e.preventDefault()
-    }
-
-
+  const handleGenerate = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+  }
 
   return (
     <div className="min-h-screen text-white p-6 md:p-12 ">
-      <form onSubmit={handelGenerate} className="max-w-4xl mx-auto mb-40">
+      <form onSubmit={handleGenerate} className="max-w-4xl mx-auto mb-40">
         <Title heading="Create In-Context Images" description="Upload your model and product images to generate stunning UGC, short-form videos and social media posts" />
-
         <div className="flex gap-20 max-sm:flex-col items-start justify-between">
           {/* Left COL */}
           <div className="flex flex-col w-full sm:max-w-60 gap-8 mt-8 mb-12">
-            <UploadZone label="Product Image" file={ productImage } onClear={() => setProductImage(null)} onChange={(e) => handleFileChange(e, 'product')} />
-              <UploadZone label="Model Image" file={ modelImage } onClear={() => setModelImage(null)} onChange={(e) => handleFileChange(e, 'model')} />
+
+            <UploadZone label="Product Image" file={productImage} onClear={() => setProductImage(null)} onChange={(e) => handleFileChange(e, 'product')} />
+
+            <UploadZone label="Model Image" file={modelImage} onClear={() => setModelImage(null)} onChange={(e) => handleFileChange(e, 'model')} />
           </div>
+
           {/* Right COL */}
-          <div>
-            <p>right col</p>
+          <div className="w-full">
+
+            <div className="mb-4 text-gray-300">
+              <label htmlFor="name"> Project Name</label>
+              <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name Your Project" required className="w-full bg-white/3 rounded-lg border-2 p-4 text-sm border-violet-200/10 focus:border-violet-500/50 outline-none transition-all mt-4" />
+            </div>
+
+
+            <div className="mt-4 text-gray-300">
+              <label htmlFor="productName"> Product Name</label>
+              <input type="text" id="productName" value={productName} onChange={(e) => setProductName(e.target.value)} placeholder="Enter Your Product Name" required className="w-full bg-white/3 rounded-lg border-2 p-4 text-sm border-violet-200/10 focus:border-violet-500/50 outline-none transition-all mt-4" />
+            </div>
+
+            <div className="mt-4 text-gray-300">
+              <label htmlFor="productDescription"> Product Description <span className="text-xs text-violet-400">(optional)</span></label>
+              <textarea id="productDescription" rows={4 } value={productDescription} onChange={(e)=>setProductDescription(e.target.value)} placeholder="Enter the description of the product" className="w-full mt-4 bg-white/3 rounded-lg border-2 p-4 text-sm border-violet-200/10 focus:border-violet-500/50 outline-none resize-none transition-all" />
+            </div>
+            <div className="mt-4 text-gray-300">
+              <label className="block text-sm mb-4">Aspect Ratio</label>
+              <div className="flex gap-3">
+                <RectangleVertical onClick={()=>setAspectRatio('9:16')} className={`p-2.5 size-13 bg-white/6 rounded transition-all ring-2 ring-transparent cursor-pointer ${aspectRatio == '9:16' ? 'ring-violet-500/50 bg-white/10' : ''}`}/>
+                  <RectangleHorizontal onClick={()=>setAspectRatio('16:9')} className={`p-2.5 size-13 bg-white/6 rounded transition-all ring-2 ring-transparent cursor-pointer ${aspectRatio == '16:9' ? 'ring-violet-500/50 bg-white/10' : ''}`}/>
+              </div>
+            </div>
+
           </div>
         </div>
 
@@ -53,4 +77,4 @@ const Genetator = () => {
   )
 }
 
-export default Genetator
+export default Generator
